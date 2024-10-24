f49019e4279c88e7eeb5db4d4dedbbf797f17446
HanSeonWoo <gkstjsdn10@gmail.com>
2024-10-24 22:40:40 +0900

    feat: admin 통신

--- index.js
+++ index.js
@@ -2,37 +2,51 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { InoutHandler } from "./src/handlers/inoutHandler.js";
-import { WebrtcHandler } from "./src/handlers/webrtcHandler.js";
+import { SignalingHandler } from "./src/handlers/signalingHandler.js";
 import { RoomManager } from "./src/services/roomManager.js";
 import { ROOM_NAMES } from "./src/utils/constants.js";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "https://rtc-admin.vercel.app/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const userSocketMap = new Map(); // {userName: {socketId, status, room}}
 
 const roomManager = new RoomManager(io, userSocketMap);
-const webrtcHandler = new WebrtcHandler(io, userSocketMap, roomManager);
+const signalingHandler = new SignalingHandler(io, userSocketMap, roomManager);
 const inoutHandler = new InoutHandler(io, userSocketMap, roomManager);
 
 io.on("connection", (socket) => {
   console.log("🚀 소켓이 연결되었습니다:", socket.id);
 
+  socket.on("admin", () => socket.join(ROOM_NAMES.ADMIN));
+  socket.on("adminMessage", ({ roomName, message }) => {
+    console.log("🚀 ~ socket.on ~ message:", roomName, message);
+    socket.to(roomName).emit("adminMessage", message);
+  });
+
   // 대기실 입장 처리
   socket.on("waiting", (userName) =>
     inoutHandler.handleWaiting(socket, userName)
   );
+  socket.on("callEnd", () => inoutHandler.handleCallEnd(socket));
 
   // WebRTC 시그널링 처리
   socket.on("offer", ({ to, offer }) =>
-    webrtcHandler.handleOffer(socket, to, offer)
+    signalingHandler.handleOffer(socket, to, offer)
   );
-  socket.on("answer", (answer) => webrtcHandler.handleAnswer(socket, answer));
+  socket.on("answer", (answer) =>
+    signalingHandler.handleAnswer(socket, answer)
+  );
   socket.on("ice", (candidate) =>
-    webrtcHandler.handleIceCandidate(socket, candidate)
+    signalingHandler.handleIceCandidate(socket, candidate)
   );
-  socket.on("callEnd", () => webrtcHandler.handleCallEnd(socket));
 
   // 채팅 처리
   socket.on("chat", ({ from, message, timestamp }) =>
